33d201bcce7591921d4a0ec4ee7de7775094f1d1
Marcus Paulo M Dias <marcus@goatme.app>
2022-06-27 19:02:58 -0300

    feat: alters, average, sd and more

--- index.ts
+++ index.ts
@@ -5,15 +5,22 @@ interface User {
   isAdmin: boolean;
   isSpectator: boolean;
 }
+
+interface Average {
+  average: number;
+  sd: number
+}
 interface ServerToClientEvents {
   roomDetail: (room: any) => void;
   userJoined: (user: User) => void;
   userLeft: (id: string) => void;
   voted: (id: string) => void;
-  clear: () => void;
+  clear: (data: any) => void;
   id: (id: string) => void;
   showVotes: (data: any) => void;
   isSpectator: (data: User) => void;
+  newRoom: (id: string) => void;
+  sd: (data: Average) => void;
 }
 interface ClientToServerEvents {
   joinRoom: (data: any) => void;
@@ -21,6 +28,7 @@ interface ClientToServerEvents {
   clear: () => void;
   showVotes: () => void;
   isSpectator: () => void;
+  newRoom: () => void;
 }
 
 import { createServer } from "http";
@@ -40,18 +48,25 @@ const rooms = new Map<string, Array<User>>();
 const userRoom = new Map<string, string>();
 
 io.on('connection', (socket) => {
-  console.log('connected');
-  console.log(socket.id);
   socket.emit('id', socket.id);
 
+  socket.on('newRoom', () => {
+    let roomId = Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
+    const existsRoom = rooms.has(roomId);
+    if (existsRoom) {
+      roomId = roomId+'1'
+    }
+    socket.emit('newRoom', roomId);
+  })
+
   socket.on('joinRoom', (data) => {
-    socket.join(data.room);
+    socket.join(data.room)
     let joinedUser: User = {
       id: socket.id,
       isVoted: false,
       isAdmin: false,
       isSpectator: false
-    };
+    }
     if (!rooms.has(data.room)) {
       joinedUser.isAdmin = true; 
       rooms.set(data.room, [joinedUser]);
@@ -65,7 +80,6 @@ io.on('connection', (socket) => {
         delete user.vote
         return user
       }));
-    console.log('joinRoom');
   })
 
   socket.on("disconnect", () => {
@@ -81,15 +95,14 @@ io.on('connection', (socket) => {
         allVoted(socket, roomToRemoveUser)
       }
     }
-    console.log("disconnected")
   })
 
   socket.on('vote', (data) => {
     const room = userRoom.get(socket.id)!
-    const user = rooms[room].findIndex(r => r.id === socket.id)
+    const user = rooms.get(room)!.findIndex((u: User) => u.id === socket.id)
     if (user != -1) {
-      rooms[room][user].vote = data
-      rooms[room][user].isVoted = true
+      rooms.get(room)![user].vote = data
+      rooms.get(room)![user].isVoted = true
     }
     socket.to(room).emit('voted', socket.id);
     socket.emit('voted', socket.id);
@@ -98,8 +111,8 @@ io.on('connection', (socket) => {
 
   socket.on('showVotes', () => {
     const room = userRoom.get(socket.id)!
-    const userIndex = rooms[room].findIndex(r => r.id === socket.id)
-    const user = rooms[room][userIndex]
+    const userIndex = rooms.get(room)!.findIndex(r => r.id === socket.id)
+    const user = rooms.get(room)![userIndex]
     if (user.isAdmin) {
       showVotes(socket, room)
     }
@@ -107,45 +120,84 @@ io.on('connection', (socket) => {
 
   socket.on('clear', () => {
     const room = userRoom.get(socket.id)!
-    const userIndex = rooms[room].findIndex(r => r.id === socket.id)
-    const user = rooms[room][userIndex]
+    const userIndex = rooms.get(room)!.findIndex(r => r.id === socket.id)
+    const user = rooms.get(room)![userIndex]
     if (user.isAdmin) {
-      rooms[room] = rooms[room].map(r => {
+      rooms[room] = rooms.get(room)!.map(r => {
         r.vote = null
         r.isVoted = false
         return r
       })
-      socket.emit('clear');
+      socket.to(room).emit('clear', rooms.get(room));
+      socket.emit('clear', rooms.get(room));
     }
     
   })
 
   socket.on('isSpectator', () => {
-    const room = userRoom.get(socket.id)!
-    const userIndex = rooms[room].findIndex(r => r.id === socket.id)
-    const user = rooms[room][userIndex]
-    user.isSpectator = !user.isSpectator
-    rooms[room][userIndex] = user
-    socket.emit('isSpectator', user);
+    try {
+      const room = userRoom.get(socket.id)!
+      const userIndex = rooms.get(room)!.findIndex(r => r.id === socket.id)
+      const user = rooms.get(room)![userIndex]
+      user.isSpectator = !user.isSpectator
+      user.vote = null
+      user.isVoted = false
+      rooms.get(room)![userIndex] = user
+      socket.to(room).emit('isSpectator', user);
+      socket.emit('isSpectator', user);
+    } catch (error) {
+      console.log(error)
+    }
   })
 
   const showVotes = (socket: Socket, room: string) => {
     if (rooms.get(room) === undefined) {
       return
     }
-    const votes = rooms[room]
+    const votes = rooms.get(room)!
     socket.to(room).emit('showVotes', votes);
     socket.emit('showVotes', votes);
+    const average = sd(room)
+    socket.to(room).emit('sd', average);
+    socket.emit('sd', average);
   }
 
   const allVoted = (socket: Socket, room: string) => {
     if (rooms.get(room) === undefined) {
       return
     }
-    const finded = rooms[room].findIndex((user: User) => user.isVoted === false)
+    const finded = rooms.get(room)!.findIndex((user: User) => user.isVoted === false && user.isSpectator === false)
     if (finded === -1) {
       showVotes(socket, room)
     }
+  }
+
+  const sd = (room: string) =>
+  {
+    if (rooms.get(room) === undefined) {
+      return
+    }
+    const arr = rooms.get(room) || []
+    const validVotes = arr.filter(user => user.isSpectator === false  && parseFloat(user.vote!) != NaN)
+
+    const sum = validVotes.reduce((acc, user) => {
+        const v = parseFloat(user.vote!) || 0
+        acc += v
+      return acc
+    }, 0)
+    const divider = validVotes.length
+    if (divider === 0) {
+      return { sd: 0, average: 0 }
+    }
+    const average = sum / divider
+    const sumDistance = validVotes.reduce((acc, user) => {
+      const v = parseFloat(user.vote!) || 0
+      const d = Math.pow(v - average, 2)
+      acc += d
+      return acc
+    }, 0)
+    const mediaDistance = sumDistance / divider
+    return { sd: Math.round( Math.sqrt(mediaDistance) * 100) / 100, average: Math.round(average) }
   }
 })
 
